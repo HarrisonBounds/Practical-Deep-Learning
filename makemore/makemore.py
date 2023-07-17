b7f9ee21872b164bf837fec26298ed7b4ccd54ce
HarrisonBounds <harrison.bounds777@gmail.com>
2023-07-17 10:34:47 -0500

    summary function of how makemore works

--- makemore/makemore.py
+++ makemore/makemore.py
@@ -2,9 +2,10 @@
 #Bigram - A piar of consecutive characters/words
 
 import torch
+import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
-words = open('Practical-Deep-Learning/makemore/names.txt', 'r').read().splitlines() #Puts each name in the text file into a list of words
+words = open('makemore/names.txt', 'r').read().splitlines() #Puts each name in the text file into a list of words
 
 N = torch.zeros((27, 27), dtype=torch.int32) #Using PyTorch to set a 27x27 (26 letter of the alphabet + start and end token) array with the type of a 32 bit integer
 
@@ -43,6 +44,63 @@ for i in range(50): #Looping multiple times to get sample multiple names
             break
     
     print(''.join(out))
+
+def summary():#Use the name 'emma' as an example
+
+    #Create a training set of bigrams
+    xs, ys = [], [] #Input data and labels for the input data (what character comes next)
+
+    #Take the first word as an example
+    for w in words[:1]:
+        chs = ['.'] + list(w) + ['.'] #Construct the beginning and end tokens
+        for ch1, ch2 in zip(chs, chs[1:]): #Organize the bigrams to get their index
+            ix1 = stoi[ch1]
+            ix2 = stoi[ch2]
+            print(ch1, ch2)
+            #Use the indexes as input and label data
+            xs.append(ix1)
+            ys.append(ix2)
+
+    xs = torch.tensor(xs)
+    ys = torch.tensor(ys)
+
+    print('xs: ', xs)
+    print('ys: ', ys)
+
+    #Randomly initialze the 27 neurons weights
+    W = torch.randn((27, 27), generator=g) #Each neuron has 27 inputs
+
+    #Forward pass : All of these layers make it easy for us to backpropogate through
+    xenc =F.one_hot(xs, num_classes=27).float() #input to the nn using one hot encoding
+    logits = xenc @ W #predict log-counts : multiply and add the x encodings by the weights
+    #Softmax : Take ouputs of a nn and outputs probability distributions
+    counts = logits.exp() #equivalent to N
+    probs = counts / counts.sum(1, keepdims=True) #normalizing distribution : probabilities for next character
+
+    nlls = torch.zeros(5)
+
+    for i in range(5):
+        #i-th bigram
+        x = xs[i].item() #input character index
+        y = ys[i].item() #label for x
+        print("============")
+        print(f'Bigram example {i+1} | {itos[x]}{itos[y]} | (indexes: {x}, {y})')
+        print(f'Input to the NN: {itos[x]} | index: {x}')
+        print(f'Output probability: {probs[i]}')
+        print(f'Label (next character): {itos[y]} | index: {y}')
+        p = probs[i, y]
+        print(f'Probability assigned by the NN to the next correct character: {p.item()}')
+        logp = torch.log(p)
+        print(f'log likelihood: {logp.item()}')
+        nll = -logp
+        print(f'negative log likelihood: {nll}')
+        nlls[i] = nll
+        print("============")
+
+        print(f'Loss/ average negative log likelihood {nlls.mean().item()}')
+
+
+summary()    
 
 
 def plot():
